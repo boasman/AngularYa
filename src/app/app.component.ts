ca8442e5d86ffbb4377f1349ec4f6e43136919ef
Elvis Boasman <Elvisboasman@gmail.com>
2024-09-07 10:43:56 +0200

    seguir trabajando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'proyecto1';
+
+  nombre = 'Rodriguez'
+  edad = 15;
+  email = "elvisbaosman@gmail.com";
+  sueldos =[1700,1600,1900];
+  activo = true;
+  sitio='http://www.google.com';
+
+  esActivo(){
+    if(this.activo){
+      return 'Trabajador Activo'
+    }
+    else{
+      return 'Trabajador Inactivo'
+    }
+  }
+
+  ulitmoTresSueldos(){
+    let suma = 0
+    for(let x =0; x < this.sueldos.length; x++)
+      suma +=this.sueldos[x];
+    return suma;    
+  }
 }
