61a45fa87442acc0409ceb1a3b1060aabe10b795
elvis michael paulino <epaulino@rauroszm.com>
2024-09-09 14:40:48 +0200

    Se termino la seccion 10 del tutorial AngularYa

--- Componente/src/app/app.component.ts
+++ Componente/src/app/app.component.ts
@@ -1,40 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DadoComponent } from "./components/dado/dado.component";
+import { CronometroComponent } from "./components/cronometro/cronometro.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DadoComponent],
+  imports: [RouterOutlet, DadoComponent, CronometroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Componente';
 
-  resultado : string = '';
+  // resultado : string = '';
 
-  valor1: number = 0;
-  valor2: number =0;
-  valor3: number =0;
+  // valor1: number = 0;
+  // valor2: number =0;
+  // valor3: number =0;
 
-  retornarAleatorio(){
-    return Math.floor(Math.random() * 6 + 1);
-  }
+  // retornarAleatorio(){
+  //   return Math.floor(Math.random() * 6 + 1);
+  // }
 
-  tirar(){
+  // tirar(){
     
-    this.valor1 = this.retornarAleatorio();
-    this.valor2 = this.retornarAleatorio();
-    this.valor3 = this.retornarAleatorio();
+  //   this.valor1 = this.retornarAleatorio();
+  //   this.valor2 = this.retornarAleatorio();
+  //   this.valor3 = this.retornarAleatorio();
 
-    if(this.valor1 === this.valor2 && this.valor2 === this.valor3)
-    {
-      this.resultado = "Ha Ganado!";
-    }
-    else
-    {
-      this.resultado = "Ha Perdido!";
-    }
+  //   if(this.valor1 === this.valor2 && this.valor2 === this.valor3)
+  //   {
+  //     this.resultado = "Ha Ganado!";
+  //   }
+  //   else
+  //   {
+  //     this.resultado = "Ha Perdido!";
+  //   }
+  // }
+
+  mensaje = '';
+
+  actualizar(numero:number){
+    this.mensaje = numero + '(Se actualiza cada 10 segundos)';
   }
 }
--- Componente/src/app/components/cronometro/cronometro.component.ts
+++ Componente/src/app/components/cronometro/cronometro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cronometro',
@@ -9,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class CronometroComponent {
 
+  segundo = 0;
+  @Input() inicio: number=0;
+  @Output() multiplo10 = new EventEmitter<number>();
+
+  ngOnInit(){
+    this.segundo = this.inicio;
+    setInterval(() => {
+      this.segundo++;
+      if(this.segundo % 10 == 0){
+        this.multiplo10.emit(this.segundo);
+      }
+    }, 1000);
+  }
 }
