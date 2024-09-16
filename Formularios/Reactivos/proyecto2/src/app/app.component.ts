3fc02f85377a59ffa8faeffc017b661e1ef54239
elvis michael paulino <epaulino@rauroszm.com>
2024-09-16 15:00:15 +0200

    Se termino el ejercicoi para hacer los formularios con formGroup

--- Formularios/Reactivos/proyecto2/src/app/app.component.ts
+++ Formularios/Reactivos/proyecto2/src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto2';
+  datos='';
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl(),
+    mail:new FormControl(),
+    mensaje: new FormControl(),    
+  })
+
+  submit(){
+    
+    //para mostrar el formulario en el navegador
+    this.datos =  `Nombre=${this.formularioContacto.value.nombre}<br>
+                  Mail=${this.formularioContacto.value.mail}<br>
+                  Mensaje=${this.formularioContacto.value.mensaje}`;
+
+    this.limpiarTextbox();       
+            
+  }
+
+  limpiarTextbox(){
+    this.formularioContacto.reset();  
+  }
+
 }
+
