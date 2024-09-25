7c42bdf539a8d8af80752bd977407386ce015f97
elvis michael paulino <epaulino@rauroszm.com>
2024-09-25 11:18:35 +0200

    Seguir trabajando con la route

--- Rutas/DefinicionRuta/src/app/app.routes.ts
+++ Rutas/DefinicionRuta/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'contacto',
+        component: ContactoComponent
+    },
+    {
+        path: 'acerca'
+    }
+];
