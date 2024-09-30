95bb7ad478492d8d72b723644470a5bd14b67ef5
elvis michael paulino <epaulino@rauroszm.com>
2024-09-30 16:08:48 +0200

    Se completo el ejercicio de ruta no existente

--- Rutas/capturaRutaNoExistente/src/app/app.routes.ts
+++ Rutas/capturaRutaNoExistente/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { Pagina404Component } from './components/pagina404/pagina404.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:InicioComponent
+    },   
+    {
+        path: 'inicio',
+        component: InicioComponent
+    },
+    {
+        path:'mapa',
+        component: MapaComponent
+    },
+    {
+        path:'contacto',
+        component: ContactoComponent
+    },
+    {
+        path:'acercade',
+        component:AcercaDeComponent
+    },
+    {
+        path:'pagina404',
+        component:Pagina404Component
+    },
+    {
+        path:'**',
+        redirectTo: 'pagina404'
+    }
+];
