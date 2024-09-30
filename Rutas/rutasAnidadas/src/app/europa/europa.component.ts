049ec5ed3620f344e514e5e942cf3eefacd4a9c8
elvis michael paulino <epaulino@rauroszm.com>
2024-09-30 16:23:07 +0200

    se crearon los componentes para america y europa

---
+++ Rutas/rutasAnidadas/src/app/europa/europa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-europa',
+  standalone: true,
+  imports: [],
+  templateUrl: './europa.component.html',
+  styleUrl: './europa.component.css'
+})
+export class EuropaComponent {
+
+}
