d4fefd980c1d8c73d4ab0b4dc7ba5533fa161a10
elvis michael paulino <epaulino@rauroszm.com>
2024-10-02 10:26:39 +0200

    Revisar porque no funciona la app

--- Rutas/rutasAnidadas/src/app/app.routes.ts
+++ Rutas/rutasAnidadas/src/app/app.routes.ts
@@ -12,39 +12,37 @@ export const routes: Routes = [
     {
         path: 'america',
         component: AmericaComponent,
-        children:[
-            {
-                path: 'chile',
-                component: ChileComponent
-            },
-            {
-                path: 'argentina',
-                component: ArgentinaComponent
-            }, 
-            {
-                path: 'uruguay',
-                component: UruguayComponent
-            }            
-        ]   
-
-    
-    },
-    {
+        children: [
+          {
+            path: 'chile',
+            component: ChileComponent
+          },
+          {
+            path: 'argentina',
+            component: ArgentinaComponent
+          },
+          {
+            path: 'uruguay',
+            component: UruguayComponent
+          }
+        ]
+      },
+      {
         path: 'europa',
         component: EuropaComponent,
         children: [
-            {
-                path: 'espana',
-                component: EspanaComponent
-            },
-            {
-                path: 'francia',
-                component: FranciaComponent
-            },
-            {
-                path: 'italia',
-                component: ItaliaComponent
-            }
+          {
+            path: 'espana',
+            component: EspanaComponent
+          },
+          {
+            path: 'francia',
+            component: FranciaComponent
+          },
+          {
+            path: 'italia',
+            component: ItaliaComponent
+          }
         ]
-    }
+      }
 ];
