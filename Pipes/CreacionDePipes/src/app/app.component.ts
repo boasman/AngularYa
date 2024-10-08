e2c9cc168cfa38b9de3bac148d7a7272523dba58
elvis michael paulino <epaulino@rauroszm.com>
2024-10-08 09:28:00 +0200

    Se termino el proyecto de pipes

--- Pipes/CreacionDePipes/src/app/app.component.ts
+++ Pipes/CreacionDePipes/src/app/app.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LetrasPipe } from './letras.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, LetrasPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CreacionDePipes';
+
+  vector = [1,2,3,4,5,6,7];
 }
