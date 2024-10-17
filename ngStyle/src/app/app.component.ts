b2a27bb8bcd7eef126de2df781ae5e3340399a36
elvis michael paulino <epaulino@rauroszm.com>
2024-10-17 14:00:55 +0200

    completar el ejercicio de ngStyle

---
+++ ngStyle/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ngStyle';
+
+  colorEstado='#f00';
+
+  retornarColor() {
+    return '#00f';
+  }
+}
